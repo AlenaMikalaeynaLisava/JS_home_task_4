1f9498fefc95fd9743f06d91a4e2d589bf01bab3
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-05-10 20:25:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const shopCustomers = [ 
-    { name: "Rachel", cashBalance: 50, desiredAlcoholItem: "vine", amountOfAlcohol: 1,  age: 18}, 
-    { name: "Monica", cashBalance: 120, desiredAlcoholItem: "vodka", amountOfAlcohol: 1.5,  age: 28}, 
-    { name: "Phoebe", cashBalance: 30, desiredAlcoholItem: "brandy", amountOfAlcohol: 10,  age: 35}, 
-    { name: "Joey", cashBalance: 45, desiredAlcoholItem: "vodka", amountOfAlcohol: 7,  age: 15}, 
-    { name: "Chandler", cashBalance: 50, desiredAlcoholItem:"cognac", amountOfAlcohol: 2,  age: 47},
-    { name: "Ross",  cashBalance: 150, desiredAlcoholItem: "rum", amountOfAlcohol: 3,  age: 34}, 
-    { name: "Sarri",cashBalance: 100, desiredAlcoholItem: "vine", amountOfAlcohol: 12,  age: 28}, 
-    { name: "Villy", cashBalance: 130, desiredAlcoholItem: "brandy", amountOfAlcohol: 1,  age: 38}, 
-    { name: "Mari",cashBalance: 160, desiredAlcoholItem: "rum", amountOfAlcohol: 9,  age: 78},
-    { name: "Hanna", cashBalance: 10, desiredAlcoholItem: "cognac", amountOfAlcohol: 4,  age: 19}, 
+    { name: "Rachel", cashBalance: 15, desiredAlcoholItem: "vine", amountOfAlcohol: 1,  age: 18}, 
+    { name: "Monica", cashBalance: 22, desiredAlcoholItem: "vodka", amountOfAlcohol: 1.5,  age: 28}, 
+    { name: "Phoebe", cashBalance: 30, desiredAlcoholItem: "brandy", amountOfAlcohol: 2,  age: 35}, 
+    { name: "Joey", cashBalance: 45, desiredAlcoholItem: "vodka", amountOfAlcohol: 1,  age: 15}, 
+    { name: "Chandler", cashBalance: 25, desiredAlcoholItem:"cognac", amountOfAlcohol: 2,  age: 47},
+    { name: "Ross",  cashBalance: 15, desiredAlcoholItem: "rum", amountOfAlcohol: 3,  age: 34}, 
+    { name: "Sarri",cashBalance: 14, desiredAlcoholItem: "vine", amountOfAlcohol: 2,  age: 28}, 
+    { name: "Villy", cashBalance: 19, desiredAlcoholItem: "brandy", amountOfAlcohol: 1,  age: 38}, 
+    { name: "Mari",cashBalance: 15, desiredAlcoholItem: "rum", amountOfAlcohol: 3,  age: 78},
+    { name: "Hanna", cashBalance: 10, desiredAlcoholItem: "cognac", amountOfAlcohol: 2,  age: 19}, 
     ];
 
-const priceForOneItem = {vine: 12.3, vodka: 8.5, rum: 17.3, cognac: 23.4, brandy: 20.3,};
- 
+const priceForOneItem = {vine: 12, vodka: 8, rum: 17, cognac: 23, brandy: 20,};
+let difference = {};
+Object.assign(difference, priceForOneItem);
+let NotEnoughMoney=[];
+
+
 function INIT(array){
   array.forEach(element => {
-    //console.log(element);
     function Customer(customer){
-    let {name, cashBalance, desiredAlcoholItem, amountOfAlcohol, age}= customer;
+      let {name, cashBalance, desiredAlcoholItem, amountOfAlcohol, age}= customer;
       let CustomerAge = (val) => val>=18?true:false;
       let CustomerAgeVar= CustomerAge(age);
       let CustomerCostForAlcoholItem =(val1) => priceForOneItem[val1];
@@ -30,10 +33,54 @@ function INIT(array){
         customer.cashBalance = +(cashBalance-CustomerCostForAlcoholPurchaseVar).toFixed(2);
         console.log(name +", your order is: " + desiredAlcoholItem + " in amount of " + amountOfAlcohol+"."+ " Your bill is "+ CustomerCostForAlcoholPurchaseVar + " rubles");
       } else if(!CustomerAgeVar){console.log( name + ", You are too young for buying alcohol")}
-       else{console.log( name + ", Your cash Balance is " + cashBalance + " that is not anough to pay your bill " + CustomerCostForAlcoholPurchaseVar)}
+       else{
+         console.log( name + ", Your cash Balance is " + cashBalance + " that is not anough to pay your bill " + CustomerCostForAlcoholPurchaseVar);
+         NotEnoughMoney.push(element);
+      }
     }   
     return(Customer(element));
   });
 }
-//console.log(shopCustomers);
 INIT(shopCustomers);
+
+
+function INIT1(array){
+  let i=0;
+  let sum=0;
+  let k=0;
+  let spirit;
+  let group="";
+  array.forEach(element => {
+    function Customer1(customer){
+    let {name, cashBalance}= customer;
+    function AmountOfGroupManey(temp){
+      if((i<2)&&(k < array.length)){
+         sum=sum+temp;
+         group=group+" "+name;
+        i++;
+        }
+        else{
+          sum=sum+temp;
+          group=group+" "+name;
+          let MinDifference = sum;
+          for (let key in difference) {
+            if( ( (sum - difference[key]) >0) && ( (sum - difference[key]) < MinDifference) ){
+              MinDifference = sum - difference[key];
+              spirit = key;
+            }
+            }
+            console.log(group);
+            console.log(spirit);
+            i=0;
+            sum=0;
+            group ='';
+          };
+        }
+        k++; 
+    AmountOfGroupManey(cashBalance);
+      }
+    return(Customer1(element));
+ 
+})
+}
+INIT1(NotEnoughMoney);
